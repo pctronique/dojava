39020c41ec7ded428efc52f4a54e0003e52bf791
pctronique <pctronique@free.fr>
2024-06-05 21:19:06 +0200

    add cmake java

--- .docker/images/linux_java/importdata.sh
+++ .docker/images/linux_java/importdata.sh
@@ -5,11 +5,6 @@ then
     JAVA_FOLDER_PROJECT=/home/project/
 fi
 
-if [ -z ${JAVA_FOLDER_WWW} ]
-then
-    JAVA_FOLDER_WWW=/home/project/.webapps
-fi
-
 if [ -z ${JAVA_FOLDER_LOG} ]
 then
     JAVA_FOLDER_LOG=/var/log/docker/java/
@@ -25,11 +20,11 @@ then
     JAVA_FOLDER_INIT_DATA=/docker-entrypoint-initdata.d/
 fi
 
-if [ ! -d "${JAVA_FOLDER_WWW}/${JAVA_FOLDER_DATA}" ]; then
+if [ ! -d "${JAVA_FOLDER_PROJECT}/${JAVA_FOLDER_DATA}" ]; then
     if [ ! -z "$(ls -A ${JAVA_FOLDER_INIT_DATA})" ]; then
-        mkdir -p "${JAVA_FOLDER_WWW}/${JAVA_FOLDER_DATA}" 2>> ${JAVA_FOLDER_LOG}/installdata.log
-        cp -r ${JAVA_FOLDER_INIT_DATA}/* "${JAVA_FOLDER_WWW}/${JAVA_FOLDER_DATA}/" 2>> ${JAVA_FOLDER_LOG}/installdata.log
-        chmod 777 -R "${JAVA_FOLDER_WWW}/${JAVA_FOLDER_DATA}"
+        mkdir -p "${JAVA_FOLDER_PROJECT}/${JAVA_FOLDER_DATA}" 2>> ${JAVA_FOLDER_LOG}/installdata.log
+        cp -r ${JAVA_FOLDER_INIT_DATA}/* "${JAVA_FOLDER_PROJECT}/${JAVA_FOLDER_DATA}/" 2>> ${JAVA_FOLDER_LOG}/installdata.log
+        chmod 777 -R "${JAVA_FOLDER_PROJECT}/${JAVA_FOLDER_DATA}"
     fi
 fi
 
--- .docker/images/linux_java/modifname.sh
+++ .docker/images/linux_java/modifname.sh
@@ -10,10 +10,15 @@ then
     JAVA_NAME_PROJECT=project
 fi
 
+if [ -e ${JAVA_FOLDER_PROJECT}/CMakeLists.txt ]
+then
+  sed -i "s/set(JAR_NAME .*/set(JAR_NAME ${JAVA_NAME_PROJECT})/" "${JAVA_FOLDER_PROJECT}/CMakeLists.txt"
+fi
+
 if [ -e ${JAVA_FOLDER_PROJECT}/build.xml ]
 then
   sed -i "s/project name=\".*\"/project name=\"${JAVA_NAME_PROJECT}\"/" "${JAVA_FOLDER_PROJECT}/build.xml"
-  sed -i "s/Builds, tests, and runs the project .*\./Builds, tests, and runs the project ${JAVA_FOLDER_PROJECT}./" "${JAVA_FOLDER_PROJECT}/build.xml"
+  sed -i "s/Builds, tests, and runs the project .*\./Builds, tests, and runs the project ${JAVA_NAME_PROJECT}./" "${JAVA_FOLDER_PROJECT}/build.xml"
 fi
 
 if [ -e ${JAVA_FOLDER_PROJECT}/nbproject/project.xml ]
@@ -26,6 +31,15 @@ then
   sed -i "s/default=\"default\" name=\".*-impl\"/default=\"default\" name=\"${JAVA_NAME_PROJECT}-impl\"/" "${JAVA_FOLDER_PROJECT}/nbproject/build-impl.xml"
   sed -i "s/suitename=\".*\"/suitename=\"${JAVA_NAME_PROJECT}\"/" "${JAVA_FOLDER_PROJECT}/nbproject/build-impl.xml"
   sed -i "s/-suitename .* -testname/-suitename ${JAVA_NAME_PROJECT} -testname/" "${JAVA_FOLDER_PROJECT}/nbproject/build-impl.xml"
+  sed -i "s/Cycle detected: .* was already built/Cycle detected: ${JAVA_NAME_PROJECT} was already built/" "${JAVA_FOLDER_PROJECT}/nbproject/build-impl.xml"
+fi
+
+if [ -e ${JAVA_FOLDER_PROJECT}/nbproject/project.properties ]
+then
+  sed -i "s/dist\.jar=\${dist\.dir}\/.*\.jar/dist.jar=\${dist\.dir}\/${JAVA_NAME_PROJECT}.jar/" "${JAVA_FOLDER_PROJECT}/nbproject/project.properties"
+  sed -i "s/dist\.jlink\.output=\${dist\.jlink\.dir}\/.*/dist.jlink.output=\${dist.jlink.dir}\/${JAVA_NAME_PROJECT}/" "${JAVA_FOLDER_PROJECT}/nbproject/project.properties"
+  sed -i "s/jlink\.launcher\.name=.*/jlink.launcher.name=${JAVA_NAME_PROJECT}/" "${JAVA_FOLDER_PROJECT}/nbproject/project.properties"
+  sed -i "s/application\.title=.*/application.title=${JAVA_NAME_PROJECT}/" "${JAVA_FOLDER_PROJECT}/nbproject/project.properties"
 fi
 
 exit 0
